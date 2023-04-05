c0d2bcc31c4f85149077bfe51515706b16f85a84
TasnimAnjum <114102739+TasnimAnjum@users.noreply.github.com>
2023-04-05 12:52:12 +0600

    flag done

--- Asset/js/index.js
+++ Asset/js/index.js
@@ -14,21 +14,31 @@
  
   //shape change
   var element=document.getElementById("box");
-  element.classList.add("rectangle");
-  if(element.classList=="rectangle"){
-    element.classList="circle";
+  element.classList.add("rectangle1");
+  if(element.classList=="rectangle1"){
+    element.classList="circle1";
   }
   else{
-    element.classList="rectangle";
+    element.classList="rectangle1";
   }
 };
       
-
-
-
-  function flag(){
-    var circle= document.createElement("div");
-    circle.classList.add("circle2");
-    document.getElementById("rectangle2").appendChild(circle) ;
+  // function flag(){
+  //   var circle= document.createElement("div");
+  //   circle.classList.add("circle2");
+  //   document.getElementById("rectangle2").appendChild(circle) ;
        
-  };
+  // };
+
+  function drawCircle() {
+    // Create a new circle element
+    var circle = document.createElement("div");
+
+    
+    circle.classList.add("circle");
+    
+    // Append the circle element to the rectangle element
+    document.getElementById("rectangle").appendChild(circle);
+  }
+
+  
